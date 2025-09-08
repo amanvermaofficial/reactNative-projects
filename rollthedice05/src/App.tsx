fe952bba3fd470e626f22324963782364354dbae
Aman <amn.lohaniwal@gmail.com>
2025-09-08 20:34:51 +0530

    fix issue

--- rollthedice05/src/App.tsx
+++ rollthedice05/src/App.tsx
@@ -68,7 +68,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#ffffffff',
-    alignItems: 'center',
+    alignItems: 'center', 
     justifyContent: 'center',
   },
   diceImage: {
